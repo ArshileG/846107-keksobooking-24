57ad981f4b1978a7d32c4e7b8d2f7dd6a37e8904
Archil Gogia <arshileg@gmail.com>
2021-11-10 17:36:54 +0400

    eslint fixes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 import { generateRandObj } from './data.js';
 import {createCardFromData} from './randElements.js';
-import {activateForm, disableForm} from './forms.js';
+import {disableForm} from './forms.js';
 
 const randomData = generateRandObj(3);
 disableForm();
-//activateForm();
 randomData.forEach((randomDataItem) =>{
   createCardFromData(randomDataItem);
 });
