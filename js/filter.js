d8fdf2bad2f6c58ee38b1a0b1e092d3a5c220ced
Archil Gogia <arshileg@gmail.com>
2021-11-21 18:00:45 +0400

    Added some functions for filter. Fixed RandElement.js

---
+++ js/filter.js
@@ -0,0 +1,50 @@
+const PRICETYPES = {
+  low: 10000,
+  middle: {min: 10000, max: 50000},
+  high: 50000,
+};
+
+
+function filterByType(offerItemValue, filterValue) {
+  if (offerItemValue.offer.type === filterValue || filterValue === 'any') {
+    return true;
+  }
+}
+
+function filterByPrice(array, filterValue) {
+  switch (filterValue) {
+    case 'any':
+      return true;
+    case 'low':
+      if (array.offer.price < PRICETYPES[filterValue]) {
+        return true;
+      }
+      break;
+    case 'middle':
+      if (array.offer.price < PRICETYPES[filterValue].max && array.offer.price > PRICETYPES[filterValue].min) {
+        return true;
+      }
+      break;
+    case 'high':
+      if (array.offer.price > PRICETYPES[filterValue]) {
+        return true;
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+function filterByRooms(offerItemValue, filterValue) {
+  if (offerItemValue.offer.rooms <= filterValue || filterValue === 'any') {
+    return true;
+  }
+}
+
+function filterByGuests(offerItemValue, filterValue) {
+  if (offerItemValue.offer.guests <= filterValue || filterValue === 'any') {
+    return true;
+  }
+}
+
+export {filterByType,filterByPrice,filterByRooms,filterByGuests};
